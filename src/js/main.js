29310c784daa8d792ac456fb0f69597589eea979
Sierra Evans <eva22011@byui.edu>
2025-05-24 12:54:52 -0700

    Prove 05 Almost Complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,21 @@
+import "../css/style.css"; // we can do this because we are using Vite...
+import "../css/home.css";
 import { getParkData, getInfoLinks } from "./parkService.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
 
 function setParkIntro(data) {
-  const introElement = document.querySelector(".intro");
-  introElement.innerHTML = `<h1>${data.fullName}</h1>
+  const introEl = document.querySelector(".intro");
+  introEl.innerHTML = `<h1>${data.fullName}</h1>
   <p>${data.description}</p>`;
 }
 
 function setParkInfoLinks(data) {
-  const infoElement = document.querySelector(".info");
+  const infoEl = document.querySelector(".info");
   // we have multiple links to build...so we map to transform the array of objects into an array of HTML strings.
   const html = data.map(mediaCardTemplate);
   // join the array of strings into one string and insert it into the section
-  infoElement.insertAdjacentHTML("afterbegin", html.join(""));
+  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
 }
 
 async function init() {
